2f22e9874fd12882ff1bc0bbcd2be617f73e5de6
dandickson <dan.dickson@gmail.com>
2018-04-24 08:34:42 -0500

    Initial ingest

---
+++ Send-SMS.ps1
@@ -0,0 +1,8 @@
+﻿$APIUser = '' #Flowroute API User
+$APIPass = '' | ConvertTo-SecureString -AsPlainText -Force #Flowroute API Password
+$APIURI = 'https://api.flowroute.com/v2.1/messages' #URI of Flowroute API
+$CallbackURI = '' #Callback URI for message confirmation
+
+Function Send-SMS($SMSTo, $SMSFrom, $SMSBody) {
+    Invoke-WebRequest -Uri $APIURI -Credential (New-Object System.Management.Automation.PSCredential($APIUser, $APIPass)) -Method Post -ContentType application/vnd.api+json -Body (@{to=$SMSTo;from=$SMSFrom;body=$SMSBody;dlr_callback=$CallbackURI} | ConvertTo-Json)  
+    }
